abeda235de3eb1ff98e9f64cde0f95d686739e8c
Demi-7020920 <7020920@spsd.sk.ca>
2026-09-22 14:11:55 -0600

    my code acc works

--- Interactive Scene Project/sketch.js
+++ Interactive Scene Project/sketch.js
@@ -9,14 +9,48 @@
 async function setup() {
   createCanvas(windowWidth, windowHeight);
 }
-
+let num = 0; 
 function draw() {
-  background(135,206,235);
+  
+  if (num === 0){// morning 
+    background(135,206,235);
+    fill(145,142,133);
+    quad(0, height - 100, width, height - 100, width, height, 0, height);
+    fill(255,234,0);
+    circle(width - 50, 50,100);
+  }
+  
+  else if (num === 1){// Evening
+    background(41,43,49);
+  }
+  else if (num === 2){//Afternoon
+    background(246,206,138);
+  }
+  fill(255,255,255);
+  aeroplane();
+  
+  
+  
+  
+  
+  
 
-
+  //triangle(mouseX + 50 , mouseY - 15 ,mouseX + 50 , mouseY + 15, mouseX + 80, mouseY);
+}
+function keyPressed(){
+  if (key === 'f'){
+    num += 1
+    if (num > 2){
+      num = 0
+    }
+    print(num);
+  }
+}
+function aeroplane(){
+  noStroke()
   ellipse(mouseX + 50, mouseY,80,30)
-  quad(mouseX - 50, mouseY + 15 )
+  quad(mouseX - 50, mouseY - 15, mouseX - 80, mouseY - 15,  mouseX - 80, mouseY - 10,  mouseX - 50, mouseY + 15)
+  quad(mouseX - 65, mouseY - 15, mouseX - 65, mouseY - 50, mouseX - 60, mouseY - 50, mouseX - 40, mouseY - 15 )
   rect(mouseX - 50,mouseY - 15,100,30);
   
-  //triangle(mouseX + 50 , mouseY - 15 ,mouseX + 50 , mouseY + 15, mouseX + 80, mouseY);
 }
